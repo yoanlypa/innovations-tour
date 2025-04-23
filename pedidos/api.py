7ea5644f1a859fb101fdd26cabde3975003b4a66
yoanlypa <pyoanly@gmail.com>
2025-04-23 11:06:50 +0200

    update api.py

--- pedidos/api.py
+++ pedidos/api.py
@@ -3,6 +3,10 @@
 from .models import Pedido, Tarea
 from .serializers import PedidoSerializer, TareaSerializer
 from rest_framework import viewsets, generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
 
 class PedidoViewSet(viewsets.ModelViewSet):
     queryset = Pedido.objects.all()
@@ -12,8 +16,11 @@ class TareaViewSet(viewsets.ModelViewSet):
     queryset = Tarea.objects.all()
     serializer_class = TareaSerializer
     
-class PedidoCreateAPI(generics.CreateAPIView):
-
-    queryset = Pedido.objects.all()
-    serializer_class = PedidoSerializer
-    
+class PedidoCreateAPI(APIView):
+    def post(self, request, *args, **kwargs):
+        print(request.data)  # 👈 Agrega esto para ver qué datos llegan
+        serializer = PedidoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
