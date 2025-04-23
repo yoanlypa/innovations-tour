3fd9c0c0baa684b19f417e448d82faaa65781dde
yoanlypa <pyoanly@gmail.com>
2025-04-23 11:52:58 +0200

    update manage.py.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_proyecto.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'control_radioguias.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
